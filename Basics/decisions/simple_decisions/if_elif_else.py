92e52a047b64595f006876dc14c82764704adb3d
Lucas Fidgett <fidgett.lucas@gmail.com>
2021-02-12 10:26:56 +0000

    added if_elif_else

---
+++ Basics/decisions/simple_decisions/if_elif_else.py
@@ -0,0 +1,10 @@
+userInput = input("Towards which direction should I paint? (up, down, left, right)\n")
+
+if userInput == "up":
+    print("I am painting in the upwards direction!")
+elif userInput == "down":
+    print("I am painting in the downwards direction!")
+elif userInput == "left":
+    print("I am painting in the leftwards direction!")
+else:
+    print("I am painting in the right direction!")
