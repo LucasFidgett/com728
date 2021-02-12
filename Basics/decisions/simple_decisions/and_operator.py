dd1b059523f454340cead8fb14522fcdf09bec5d
Lucas Fidgett <fidgett.lucas@gmail.com>
2021-02-12 11:40:49 +0000

    added and_operator

---
+++ Basics/decisions/simple_decisions/and_operator.py
@@ -0,0 +1,7 @@
+userSound = input("What did I hear?\n")
+userSee = input("What did I see?\n")
+
+if (userSound == "grr") and (userSee == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue")
