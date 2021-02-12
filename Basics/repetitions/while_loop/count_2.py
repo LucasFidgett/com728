30a3ed6ecd4d992f32d604bb25e1b96f825cd584
Lucas Fidgett <fidgett.lucas@gmail.com>
2021-02-12 18:05:11 +0000

    added count_2

---
+++ Basics/repetitions/while_loop/count_2.py
@@ -0,0 +1,7 @@
+userCables = int(input("How many live cables must I avoid? \n"))
+i = 0
+
+while userCables > i:
+    print(f"Avoiding... ")
+    i = i + 1
+    print(f"Done! {i} live cables avoided.")
