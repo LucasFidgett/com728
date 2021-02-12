29dc933e2524057bc0751c3534a22226e60fb26e
Lucas Fidgett <fidgett.lucas@gmail.com>
2021-02-12 10:19:39 +0000

    added if_else

---
+++ Basics/decisions/simple_decisions/if_else.py
@@ -0,0 +1,8 @@
+userInput = input("please enter the activity to be performed.\n")
+
+if userInput == "calculate":
+    print("Performing calculations...")
+else:
+    print("Performing activity!")
+
+print("Activity completed")
