69a3dc29bdad88afbf008674643aaea3464dee71
Lucas Fidgett <fidgett.lucas@gmail.com>
2021-02-17 17:17:14 +0000

    Added simple.py in for_loop

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,11 @@
+userAmount = int(input("How many mountines should I display?\n"))
+
+for i in range(userAmount):
+    print("""
+           __
+          /  \_  
+         /^    \\
+        /  ^    \_
+      _/ ^ ^     ^\\
+     /  ^     ^    \\
+    """)
