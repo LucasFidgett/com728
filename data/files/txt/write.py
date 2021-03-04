0b469013a996e0c3703758d74feea2150baba0ec
Lucas Fidgett <fidgett.lucas@gmail.com>
2021-03-04 19:29:10 +0000

    Added code to demonstrate how to write to a text file.

---
+++ data/files/txt/write.py
@@ -0,0 +1,28 @@
+def search(file_path):
+    print("Searching...")
+    sections = "\n"
+    books = "Books:\n"
+    with open(file_path) as file:
+        for line in file:
+            if line.startswith("Section"):
+                sections += line
+            else:
+                books += line
+
+    print("Done!")
+    return f"{sections}\n\n{books}"
+
+
+def save(file_path, data):
+    print("Saving...")
+    with open(file_path, "w") as file:
+        file.write(data)
+    print("Done!")
+
+
+def run():
+    data = search("books.txt")
+    save("section-books.txt", data)
+
+
+run()
--- testing.py
+++ testing.py
@@ -1,42 +1,26 @@
-# Define the function
-def commence():
-    # Lines 4 & 5 print out the statements
-    print("The Avengers Initiative")
-    print("Please enter the number of days: ")
-    # Line 6 reads the users input and stores as the variable num_of_days
-    num_of_days = int(input())
-    # Line 7 prints out the statement with the variable num_of_days using an f string
-    print(f"The Avengers Initiative will commence in {num_of_days} days.")
+def display_chars(file_path, num_of_characters):
+    with open(file_path) as file:
+        # can also do this in one step file.read(num_of_characters)
+        partial_data = file.read(num_of_characters)
+        print(partial_data)
 
 
-# Run the function
-commence()
+def display_line(file_path):
+    with open(file_path) as file:
+        first_line = file.readline().strip()
+        print(first_line)
 
 
-# Define the function passing through leader
-def lead(leader):
-    # If function, if the answer is Tony Stark the message "The Avengers are ready will display"
-    # If it is anything else the if function will print "We need a better leader."
-    if leader == "Tony Stark":
-        print("The Avengers are ready!")
-    else:
-        print("We need a better leader.")
+def display_text(file_path):
+    with open(file_path) as file:
+        data = file.read()
+        print(data)
 
 
-# Call the function passing through Hulk and Tony Stark as the string respectively
-lead("Hulk")
-lead("Tony Stark")
+def run():
+    display_chars("library.txt", 5)
+    display_line("library.txt")
+    display_text("library.txt")
 
 
-# Define the function passing through num_avengers
-def assemble(num_avengers):
-    # Print line
-    print("Assembling Avengers...")
-    # Starts a for loop starting at 0 (i) counting up to the value stored in the variable num_avengers
-    # and loops that amount of times (3). Each loop printing the string ... Avenger has assembled.
-    for i in range(num_avengers):
-        print("... Avenger has assembled.")
-
-
-# Calls the function and passes through the integer 3
-assemble(3)
+run()
